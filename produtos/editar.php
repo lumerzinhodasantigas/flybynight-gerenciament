ca65af95ed64593c2aea5820b992ede01d0cd225
Kenny <shadow.dio.shadow@gmail.com>
2025-10-14 14:16:45 -0300

    Update editar.php

--- produtos/editar.php
+++ produtos/editar.php
@@ -1,50 +1,60 @@
 <?php
+
 require_once "../src/fornecedor_crud.php";
-// Pegando da URL o valor do parâmetro chamado id
-$id = $_GET['id'];
 
-// Chamamos a função, passando dados de conexão e o id do fornecedor a ser buscado
-$fornecedor = buscarFornecedorPorId($conexao, $id);
-
-/* echo "<pre>";
-var_dump($fornecedor);
-echo "</pre>"; */
-
-if( $_SERVER['REQUEST_METHOD'] === 'POST'){
-
-    $nome = $_POST['nome'];
-    atualizarFornecedor($conexao, $nome, $id);
-
-    // Após redirecionar usando header()...
-    header("location:lista.php");
-
-    // ... sempre encerre/interrompa o script (evitando erros)
-    exit;
-}
+$fornecedores = buscarFornecedores($conexao);
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Fornecedor</title>
+    <title>Editar Produto</title>
     <link rel="stylesheet" href="../css/estilo.css">
 </head>
 <body>
-    <h1>Editar fornecedor</h1>
+    <h1>Editar Produto</h1>
 
     <form action="" method="post">
-        <div>
 
-        <input type="hidden" name="id" value="<?=$fornecedor['id']?>">
-        <label for="nome">Nome:</label>
-        <!-- Atributo required indica um campo obrigatório -->
-        <input value="<?=$fornecedor['nome']?>" type="text" name="nome" id="nome" required>
-
+         <div>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
         </div>
+
+        <div>
+            <label for="descricao">Descrição:</label>
+            <textarea name="descricao" id="descricao" rows="4"></textarea>
+        </div>
+
+        <div>
+            <label for="preco">Preço:</label>
+            <input type="number" name="preco" id="preco" required min="0" step="0.01">
+        </div>
+
+        <div>
+            <label for="quantidade">Quantidade:</label>
+            <input type="number" name="quantidade" id="quantidade" required min="0" step="0.01">
+        </div>
+
+        <div>
+            <label for="fornecedor">Fornecedor:</label>
+            <select name="fornecedor_id" id="fornecedor" required>
+
+                <option value="">-- Selecione --</option>
+
+                <?php foreach($fornecedores as $fornecedor): ?>
+                    <option value="<?=$fornecedor['id']?>"><?=$fornecedor['nome']?></option>
+                <?php endforeach; ?>
+
+            </select>
+        </div>
+
         <button type="submit"> Atualizar </button>
+
     </form>
 
-    <a href="lista.php"> ⬅ Voltar </a>
+    <a href="lista2.php"> ⬅ Voltar </a>
 
+</body>
 </html>
